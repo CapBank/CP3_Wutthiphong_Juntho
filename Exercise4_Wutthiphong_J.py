7775d958ad669ebe04637bffd74f70ec7d4d21e4
CapBank <bankwutthiphong@gmail.com>
2021-03-07 15:55:22 +0700

    Create Exercise4_Wutthiphong_J.py

---
+++ Exercise4_Wutthiphong_J.py
@@ -0,0 +1,9 @@
+FE = 50.1    #Foundation English
+GB =62.3     #General Business
+ICS =52.2    #Introduction to Computer Systems
+CP =75.6     #Computer Programming
+print("----- Your Score-----")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
