29b222fcd38cd45ae666ca7a09346b977a203200
CapBank <bankwutthiphong@gmail.com>
2021-03-15 21:05:16 +0700

    Create Lecture50_Wutthiphong_J.py

---
+++ Lecture50_Wutthiphong_J.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiNumber(x,y):
+    print(x*y)
+def divNumber(x,y):
+    print(x/y)
+addNumber(10,5)
+minusNumber(10,5)
+multiNumber(10,5)
+divNumber(10,5)
